93b88f2f48fe89866a7c5b6cf509707c507be994
MoeMA551 <92270796+MoeMA551@users.noreply.github.com>
2026-09-15 15:12:03 +0000

    version 1 output file is correct

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, send_file
 import pandas as pd 
 import os
 import re
+import openpyxl
 
 app = Flask(__name__)
 
@@ -14,6 +15,7 @@ os.makedirs(OUTPUT_FOLDER, exist_ok=True)
 @app.route("/")
 def render():
     return render_template("index.html")
+
 
 @app.route("/process", methods=["POST"])
 def process_file():
@@ -29,19 +31,24 @@ def process_file():
     input_path = os.path.join(UPLOAD_FOLDER, file.filename)
     file.save(input_path)
 
+    #Reading data from uploaded excel file
     df = pd.read_excel(input_path)
 
     if "reservation site" not in df.columns:
         return "Error: 'reservation site' column was not found."
+    
     output_path = os.path.join(
         OUTPUT_FOLDER,
         "modified_reservation.xlsx"
     )
 
+    #Writing output excel file
     with pd.ExcelWriter(output_path, engine="openpyxl") as writer:
 
+        #Groupby reservaton site
         for site, site_data in df.groupby("reservation site"):
 
+            #remove unallowed characters
             sheet_name = re.sub(
                 r'[:\\/?*\[\]]',
                 '',
@@ -55,11 +62,12 @@ def process_file():
                 sheet_name=sheet_name,
                 index=False
             )
-        
-        return send_file(
-        output_path,
-        as_attachment=True,
-        download_name="modified_reservation.xlsx"
+
+    #autodownloading the file
+    return send_file(
+    output_path,
+    as_attachment=True,
+    download_name="modified_reservation.xlsx"
     )
 
 if __name__== "__main__":
